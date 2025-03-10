e72cfded20c17765dd251e333ce415942bf7b4aa
maaria010 <159135507+maaria010@users.noreply.github.com>
2025-03-10 09:20:56 -0300

    ..

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener ( 'DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-    const aumentaFonteBotao = document.getElementById('diminuir-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function( ){
         tamanhoAtualFonte += 0.1;
